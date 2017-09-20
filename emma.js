e180ae0bffca85e8cf0d68f0d8ddd0557912857b
emmaxlang <31938633+emmaxlang@users.noreply.github.com>
2017-09-20 11:57:51 -0700

    Create emma.js

---
+++ emma.js
@@ -0,0 +1,8 @@
+var first= document.getElementById('one');
+var second= document.getElementById('two');
+var third= document.getElementById('three');
+var addbtn= document.getElementById('addbtn');
+
+addbtn.addEventListener("click". function(){
+        alert(+first.value + +second.value + +third.value)
+})
